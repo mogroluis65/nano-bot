82de7dfae3523ef5f7b03f0fac1ce1717c1237a0
Luis Mogro <luismogro65@gmail.com>
2025-07-31 23:10:24 -0300

    Cambio app.run para produccion en Reander

--- bot.py
+++ bot.py
@@ -60,5 +60,5 @@ def whatsapp_webhook():
     <Message>{respuesta}</Message>
 </Response>"""
 
-# if __name__ == "__main__":
-#    app.run(debug=True)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0')
